c5bc6d1c6d1e10998637403aa1f608c239a0be93
Inventorgithub <105780850+Inventorgithub@users.noreply.github.com>
2022-06-24 17:13:33 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS STATEMENT
+ This is a javascript code that prints out your name, height, and country on the screen
---
+++ script.js
@@ -0,0 +1,18 @@
+// 
+let myName = "Daniel";
+let myHeight = "1.7m";
+let myCountry = "Nigeria";
+alert(
+  "My name is " +
+    myName +
+    "\n" +
+    "My height is " +
+      myHeight +
+      "\n" +
+      "I am from " +
+      myCountry
+
+);
+
+
+
